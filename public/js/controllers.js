643668123d793142338e197e5130ea1b693110f9
Neil Molina <get.neilmolina@gmail.com>
2014-09-12 23:42:29 +0800

    step-5: service creation, service injection to all controllers, / and reusing user data

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,7 +1,19 @@
 (function(angular){
   var app = angular.module("CatControllers",[]); //you can define module dependencies also here if needed
 
-  app.controller("CatsCtrl",["$scope","$http",function($s,$x){
+  app.controller("MainCtrl",["$scope","User",function($s, User){
+    $s.checkUser = function(){
+      // continuously be tested by ng-if from index.html
+      return User.user ? true:false;
+    };
+    $s.register = function(user){
+      if(!User.register(user)){
+        $s.message = "please  provide the complete details"
+      };
+    };
+    $s.message = "please provide your name and email";
+  }])
+  .controller("CatsCtrl",["$scope","$http","User",function($s,$x,User){
     // $scope.message = "Hello World";
     // $scope.getMessage = function(){
     //   return "from getMessage(): "+$scope.message;
@@ -12,7 +24,15 @@
     }).success(function(cats){
       $s.cats = cats;
     })
-  }]).controller("CatCtrl",["$scope","$routeParams","$http",function($s,$rp,$x){
+
+    $s.user = User.user;
+
+    $s.$on("user:new",function(){ //listening for a custom event
+      console.log("new user added");
+      $s.user = User.user;
+    })
+  }])
+  .controller("CatCtrl",["$scope","$routeParams","$http","User",function($s,$rp,$x,User){
     //console.log($rp);
     $s.activeIndex = 0;
     $s.show = function(idx){
@@ -21,5 +41,19 @@
     $x.get("/json/cat"+$rp.id+".json").success(function(data){
         $s.cat = data;
     })
+    $s.user = User.user;
+
+    $s.$on("user:new",function(){ //listening for a custom event
+      console.log("new user added");
+      $s.user = User.user;
+    });
+    $s.bids = [];
+    $s.bid = function(bid){
+      if(bid.price && bid.message){
+        $s.newBid = {}; //terminating the current content of the bid form
+        bid.user = $s.user.name;
+        $s.bids.push(bid);
+      }
+    }
   }])
 }(window.angular));
--- public/js/services.js
+++ public/js/services.js
@@ -1,3 +1,18 @@
 (function(angular){
   var app = angular.module("CatServices",[]); //you can define module dependencies also here if needed
+
+  app.service("User",["$rootScope",function($rs){
+    this.user = null;
+
+
+    this.register = function(data){
+      var test = data.email && data.name ? true:false;
+      if(test){
+        this.user = data;
+        $rs.$broadcast("user:new");
+      }
+      return test;
+    }
+
+  }])
 }(window.angular));
